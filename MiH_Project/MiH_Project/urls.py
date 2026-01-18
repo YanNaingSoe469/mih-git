e8a662e6fff66fd2b82d548e07e68d39edda0edc
Yan Naing Soe <ysoe67131@gmail.com>
2026-01-18 21:29:47 +0630

    Quality Check for week1

--- MiH_Project/MiH_Project/urls.py
+++ MiH_Project/MiH_Project/urls.py
@@ -3,21 +3,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
     ##authentication routes
     path("admin/", admin.site.urls),
-    path("", views.login_page),
-    path("login-page/", views.login_page, name="login#page"),
-    path("register-page/", views.register_page, name="register#page"),
+    path("", TemplateView.as_view(template_name="login.html")),
     path("register/", views.register, name="register"),
     path("signout/", views.signout, name="signout"),
     path("signin/", views.signin, name="signin"),
     path("test-homepage/", views.test_homepage, name="test_homepage"),
-    path("profile/", views.profilePage, name="profile_page"),
-    path("update-profile/<int:id>/", views.updateProfile, name="update_profile"),
-    path("change-password/", views.changePassword, name="change_password"),
+    path("profile/", views.profile_page, name="profile_page"),
+    path("update-profile/<int:id>/", views.update_profile, name="update_profile"),
+    path("change-password/", views.change_password, name="change_password"),
 ]
 
 if settings.DEBUG:
