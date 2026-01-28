5234b8c7a06ca7987c45c72e94592cb42c72f036
Yan Naing Soe <ysoe67131@gmail.com>
2026-01-28 16:04:35 +0630

    Added Project CRUD v1.0

--- MiH_Project/MiH_Project/urls.py
+++ MiH_Project/MiH_Project/urls.py
@@ -1,4 +1,6 @@
-from authentication import views
+from authentication import views as authentication_views
+from projects_app import views as project_views
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -10,13 +12,22 @@ urlpatterns = [
     ##authentication routes
     path("admin/", admin.site.urls),
     path("", TemplateView.as_view(template_name="login.html")),
-    path("register/", views.register, name="register"),
-    path("signout/", views.signout, name="signout"),
-    path("signin/", views.signin, name="signin"),
-    path("test-homepage/", views.test_homepage, name="test_homepage"),
-    path("profile/", views.profile_page, name="profile_page"),
-    path("update-profile/<int:id>/", views.update_profile, name="update_profile"),
-    path("change-password/", views.change_password, name="change_password"),
+    path("register/", authentication_views.register, name="register"),
+    path("signout/", authentication_views.signout, name="signout"),
+    path("signin/", authentication_views.signin, name="signin"),
+    path("test-homepage/", authentication_views.test_homepage, name="test_homepage"),
+    path("profile/", authentication_views.profile_page, name="profile_page"),
+    path("update-profile/<int:id>/", authentication_views.update_profile, name="update_profile"),
+    path("change-password/", authentication_views.change_password, name="change_password"),
+
+    #project_app routes
+    path("project-create/", project_views.project_create, name="project_create"),
+    path("sw-create/", project_views.create_software, name="sw_create"),
+    path("hw-create/", project_views.create_hardware, name="hw_create"),
+    path("ai-create/", project_views.create_ai, name="ai_create"),
+    path('project-detail/<int:id>/', project_views.project_detail, name="project_detail"),
+    path('project-update/<int:id>/', project_views.project_update, name="project_update"),
+    path('project-delete/<int:id>/', project_views.project_delete, name="project_delete"),
 ]
 
 if settings.DEBUG:
--- MiH_Project/projects_app/models.py
+++ MiH_Project/projects_app/models.py
@@ -1,3 +1,111 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    project_types = [
+        ('SW', 'Software'),
+        ('HW', 'Hardware'),
+        ('AI', 'AI'),
+    ]
+
+    title = models.CharField(max_length=100)
+    innovator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="project_innovator",
+    )
+    cover_photo = models.ImageField(upload_to="project_covers/")
+    description = models.TextField()
+    duration = models.FloatField()
+    type = models.CharField(max_length=10, choices=project_types)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Projects"
+
+    def __str__(self):
+        return self.title
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Languages"
+
+class Framework(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Frameworks"
+
+class Software(Project):
+    platform_types = [
+        ('WEB', 'Web'),
+        ('MOB', 'Mobile'),
+        ('DES', 'Desktop'),
+        ('CRS', 'Cross Platform'),
+    ]
+    source_link = models.URLField()
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, related_name="built_with")
+    framework = models.ForeignKey(Framework, on_delete=models.CASCADE, related_name="written_with")
+    platform = models.CharField(max_length=10, choices=platform_types)
+
+    class Meta:
+        verbose_name_plural = "Software"
+
+    def __str__(self):
+        return self.title
+
+
+class Component(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Components"
+
+class Hardware(Project):
+    levels = [
+        ('BEG', 'Beginner'),
+        ('INT', 'Intermediate'),
+        ('ADV', 'Advanced'),
+    ]
+    code = models.URLField()
+    components = models.ManyToManyField(Component, related_name="consists_of")
+    skill_level = models.CharField(max_length=20, choices=levels)
+
+    class Meta:
+        verbose_name_plural = "Hardware"
+
+    def __str__(self):
+        return self.title
+
+class Focus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Focuses"
+
+class Algorithm(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Algorithms"
+
+class Ai(Project):
+    dataset_link = models.URLField()
+    notebook_source_link = models.URLField()
+    algorithms = models.ManyToManyField(Algorithm, related_name="applies")
+    focus = models.ForeignKey(Focus, on_delete=models.CASCADE, related_name="has")
+    class Meta:
+        verbose_name_plural = "Ai"
+
+    def __str__(self):
+        return self.title
