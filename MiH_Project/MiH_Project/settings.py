183c3c3fec20e5659ed6cf11f5ebdb46e84c1365
Yan Naing Soe <ysoe67131@gmail.com>
2026-05-24 13:19:27 +0630

    change: database

--- MiH_Project/MiH_Project/settings.py
+++ MiH_Project/MiH_Project/settings.py
@@ -64,8 +64,12 @@ WSGI_APPLICATION = 'MiH_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mih_db',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
